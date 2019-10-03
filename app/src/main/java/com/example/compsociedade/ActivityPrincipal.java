c03c99ab390c79cccebaaffe909f0c0bd0bd6577
root <alrakowski@inf.ufsm.br>
2019-10-03 15:47:29 -0300

    Erro nav_bar

--- app/src/main/java/com/example/compsociedade/ActivityPrincipal.java
+++ app/src/main/java/com/example/compsociedade/ActivityPrincipal.java
@@ -3,10 +3,12 @@ package com.example.compsociedade;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.Navigation;
 
 import android.view.View;
 
@@ -16,10 +18,10 @@ public class ActivityPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,4 +30,11 @@ public class ActivityPrincipal extends AppCompatActivity {
             }
         });
     }
+
+    public void showMenu(View view){
+        NavigationView n = (NavigationView) findViewById(R.id.nav_view);
+        n.se
+    }
+
+
 }
