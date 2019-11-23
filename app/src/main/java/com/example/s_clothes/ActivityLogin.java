b79e55968795968831f8f545b8311d9355b3b3fd
alrakowski <37090054+alrakowski@users.noreply.github.com>
2019-11-23 09:05:15 -0200

    ATENDA UM DESEJO

--- app/src/main/java/com/example/s_clothes/ActivityLogin.java
+++ app/src/main/java/com/example/s_clothes/ActivityLogin.java
@@ -27,6 +27,12 @@ public class ActivityLogin extends AppCompatActivity {
         startActivity(intent);
     }
 
+
+    public void abreAtendaDesejo(View view){
+        Intent intent = new Intent(this, ActivityAtendaUmDesejo.class);
+        startActivity(intent);
+    }
+
     public void openHome(View view){
         /*
          * Fazer a autenticação aqui, se falhar retorna um erro
