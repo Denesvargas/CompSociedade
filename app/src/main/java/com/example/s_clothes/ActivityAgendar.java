314ca2525b74e5463d8acdbcfa13b0fe7dd2e327
rvales <rafaelvalesb@gmail.com>
2019-11-23 20:29:44 -0300

    Add activity to get clothes on associations

--- app/src/main/java/com/example/s_clothes/ActivityAgendar.java
+++ app/src/main/java/com/example/s_clothes/ActivityAgendar.java
@@ -6,21 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class ActivityItem extends AppCompatActivity {
+public class ActivityAgendar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item);
+        setContentView(R.layout.activity_agendar);
     }
 
-    public void abreChat(View view){
-        Intent intent = new Intent(this, ActivityChat.class);
-        startActivity(intent);
-    }
-
-    public void abreAgendar(View view){
-        Intent intent = new Intent(this, ActivityAgendar.class);
+    public void onClickButton(View view) {
+        Intent intent = new Intent(this, HomeDoador.class);
         startActivity(intent);
     }
 
