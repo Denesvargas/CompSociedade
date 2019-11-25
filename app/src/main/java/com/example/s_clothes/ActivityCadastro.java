2076441669d443d1d1d66515cffd74208d082fde
alrakowski <37090054+alrakowski@users.noreply.github.com>
2019-11-25 10:30:37 -0200

    Cadastro redirecionar para Home

--- app/src/main/java/com/example/s_clothes/ActivityCadastro.java
+++ app/src/main/java/com/example/s_clothes/ActivityCadastro.java
@@ -1,6 +1,8 @@
 package com.example.s_clothes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class ActivityCadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+    }
+
+    public void abreHome(View view){
+        Intent intent = new Intent(this, HomeDoador.class);
+        startActivity(intent);
     }
 }
